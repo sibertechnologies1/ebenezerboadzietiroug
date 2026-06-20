0d9f79b66aee59f04ed5c2358e651c76e2c9289b
sibertechnologies1 <sibertechnologies1@gmail.com>
2026-06-20 14:14:16 +0000

    I have made some changes to the codebase and I am now ready to commit those changes. I will write a commit message that describes the changes I have made.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/about' element={<SkillSection />} />
+        <Route path='/about' element={<About />} />
         <Route path='/portfolio' element={<PortfolioSection />} />
         <Route path='/contact' element={<LeadMagnet />} /> */}
       </Routes>
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+function About() {
+  return (
+    <div>
+      <Navbar />
+<div className=' flex flex-col bg-white'></div>
+    </div>
+  )
+}
+
+export default About
